13a1cd33b14f1712d596445726b19773d2a7a29e
Eduardo Gomez Sanchez <54601814+edugs93@users.noreply.github.com>
2022-02-20 23:24:26 -0600

    Add files via upload

---
+++ program.py
@@ -0,0 +1,24 @@
+ # program.py
+sum = 1 + 2
+print(sum)
+print('Hola desde la consola')
+print('Probando variables')
+sum = 1 + 2 # 3
+product = sum * 2
+print(product)
+print('Tipos de Variables')
+planetas_en_el_sistema_solar = 8 # int, plutón era considerado un planeta pero ya es muy pequeño
+distancia_a_alfa_centauri = 4.367 # float, años luz
+puede_despegar = True
+transbordador_que_aterrizo_en_la_luna = "Apollo 11" #string
+print(planetas_en_el_sistema_solar)
+print(distancia_a_alfa_centauri)
+print(puede_despegar)
+print(transbordador_que_aterrizo_en_la_luna)
+    # Tip de práctica 1: Intenta ejecutarlo en un notebook.
+a = 97
+b = 55
+    # test expression / expresión de prueba
+if a < b:
+    # statement to be run / instrucción a ejecutar
+    print(b)
